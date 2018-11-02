a690eaa9ffe4c4430f559bdcf20e461eaf4e7cb2
alien190 <alien1900@bk.ru>
2018-11-02 18:04:34 +0700

    change TextView to ImageView

--- app/src/main/java/com/example/alien/sharedelementtransitions/FirstFragment.java
+++ app/src/main/java/com/example/alien/sharedelementtransitions/FirstFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.SharedElementCallback;
 import android.support.v4.view.ViewCompat;
+import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Map;
 
 public class FirstFragment extends Fragment {
 
     private ImageView mImageView;
+    //private TextView mTextView;
 
     public static FirstFragment newInstance() {
 
@@ -28,18 +35,43 @@ public class FirstFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fr_first, container, false);
         mImageView = view.findViewById(R.id.first_image);
+        //mTextView = view.findViewById(R.id.tvText);
         mImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getFragmentManager()
+                getActivity().getSupportFragmentManager()
                         .beginTransaction()
+                        .setReorderingAllowed(true)
                         .addSharedElement(mImageView,
                                 ViewCompat.getTransitionName(mImageView))
-                        .addToBackStack("TAG")
                         .replace(R.id.container, SecondFragment.newInstance())
+                        .addToBackStack("TAG")
+
                         .commit();
             }
         });
+
+        setExitTransition(TransitionInflater.from(getContext()).inflateTransition(R.transition.launch_list_exit_transition));
+        setExitSharedElementCallback(new SharedElementCallback() {
+            @Override
+            public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
+                sharedElements.put(names.get(0), mImageView);
+            }
+        });
+
+        // postponeEnterTransition();
+
+
         return view;
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+        // startPostponedEnterTransition();
+    }
+
+
 }
--- app/src/main/java/com/example/alien/sharedelementtransitions/SecondFragment.java
+++ app/src/main/java/com/example/alien/sharedelementtransitions/SecondFragment.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.SharedElementCallback;
 import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+import java.util.Map;
 
 public class SecondFragment extends Fragment {
+    private View mView;
+    private ImageView mImageView;
+
     public static SecondFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -22,14 +30,33 @@ public class SecondFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setSharedElementEnterTransition(TransitionInflater
-                .from(getContext())
-                .inflateTransition(android.R.transition.move));
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fr_second, container, false);
+        mView = inflater.inflate(R.layout.fr_second, container, false);
+        mImageView = mView.findViewById(R.id.second_image);
+        setSharedElementEnterTransition(TransitionInflater
+                .from(getContext())
+                .inflateTransition(R.transition.image_shared_element_transition));
+
+        setEnterSharedElementCallback(new SharedElementCallback() {
+            @Override
+            public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
+                sharedElements.put(names.get(0), mImageView);
+            }
+        });
+
+       // postponeEnterTransition();
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //startPostponedEnterTransition();
     }
 }
